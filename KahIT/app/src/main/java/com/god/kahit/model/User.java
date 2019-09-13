2d97ae52e53cc8d440f5d6fc5c3490fe1667a968
MiztaOak <ek.johan98@gmail.com>
2019-09-13 12:29:58 +0200

    Made some minor changes to the user class by adding som of its instance variables

--- KahIT/app/src/main/java/com/god/kahit/model/User.java
+++ KahIT/app/src/main/java/com/god/kahit/model/User.java
@@ -1,4 +1,11 @@
 package com.god.kahit.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class User {
+    private String name;
+
+    private final List<Powerups> powerups = new ArrayList<>();
+    private final List<Cosmetic> cosmetics = new ArrayList<>();
 }
