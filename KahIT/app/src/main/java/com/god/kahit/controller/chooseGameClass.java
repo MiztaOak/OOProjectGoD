31b203f31abd2919e121a13a8fbbcd556d851618
Oussama <Anadani>
2019-09-15 22:33:05 +0200

    Update chooseGameClass.java

--- KahIT/app/src/main/java/com/god/kahit/controller/chooseGameClass.java
+++ KahIT/app/src/main/java/com/god/kahit/controller/chooseGameClass.java
@@ -11,5 +11,7 @@ public class chooseGameClass extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.choose_game_activity);
+
+        
     }
 }
