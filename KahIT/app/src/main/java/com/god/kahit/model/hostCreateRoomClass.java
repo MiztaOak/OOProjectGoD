8d50bb4f238e69103110036166138378127fb2b8
Oussama <Anadani>
2019-09-15 20:22:11 +0200

    all  xml files and related classes

---
+++ KahIT/app/src/main/java/com/god/kahit/model/hostCreateRoomClass.java
@@ -0,0 +1,13 @@
+package com.god.kahit;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class hostCreateRoomClass extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.host_create_room_activity);
+    }
+}
