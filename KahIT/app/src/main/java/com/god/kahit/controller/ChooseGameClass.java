a0bcc50ce410dba795ea87d709ec7ee0b76bb3a7
Oussama <Anadani>
2019-09-16 18:52:36 +0200

    linked some buttons
    Don't forget to download recyclerView library to avoid crashing the app

--- KahIT/app/src/main/java/com/god/kahit/controller/ChooseGameClass.java
+++ KahIT/app/src/main/java/com/god/kahit/controller/ChooseGameClass.java
@@ -1,17 +1,47 @@
 package com.god.kahit.controller;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 import com.god.kahit.R;
 
 public class ChooseGameClass extends AppCompatActivity {
 
+    private static final String LOG_TAG = ChooseGameClass.class.getSimpleName();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.choose_game_activity);
+    }
 
-        
+    public void launchHostCreateRoomClass(View view){
+        Log.d(LOG_TAG, "Button clicked!");
+        Intent intent = new Intent(this, HostCreateRoomClass.class);
+        startActivity(intent);
+    }
+
+    public void launchBackMainActivityClass (View view){
+        Log.d(LOG_TAG, "Button clicked!");
+        Intent intent = new Intent(this, MainActivityClass.class);
+        startActivity(intent);
+
+    }
+
+    public void launchJoinRoomClass (View view){
+        Log.d(LOG_TAG, "Button clicked!");
+        Intent intent = new Intent(this, JoinRoomClass.class);
+        startActivity(intent);
+
+    }
+
+    public void launchHotSwapGameModeClass(View view){
+        Log.d(LOG_TAG, "Button clicked!");
+        Intent intent = new Intent(this, HotSwapGameModeClass.class);
+        startActivity(intent);
+
     }
 }
--- KahIT/app/src/main/java/com/god/kahit/controller/JoinRoomClass.java
+++ KahIT/app/src/main/java/com/god/kahit/controller/JoinRoomClass.java
@@ -1,15 +1,29 @@
 package com.god.kahit.controller;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
 
 import com.god.kahit.R;
 
 public class JoinRoomClass extends AppCompatActivity {
+
+
+    private static final String LOG_TAG = JoinRoomClass.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.join_room_activity);
     }
+
+    public void launchBackChooseGameClass (View view){
+        Log.d(LOG_TAG, "Button clicked!");
+        Intent intent = new Intent(this, ChooseGameClass.class);
+        startActivity(intent);
+
+    }
+
 }
