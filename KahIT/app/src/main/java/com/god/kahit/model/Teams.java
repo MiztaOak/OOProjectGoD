f01b4cfe4055bae210e18faab4ec3ca21888251d
MiztaOak <ek.johan98@gmail.com>
2019-09-19 14:49:40 +0200

    Added some more data that will be usefull for all developers

--- KahIT/app/src/main/java/com/god/kahit/model/Teams.java
+++ KahIT/app/src/main/java/com/god/kahit/model/Teams.java
@@ -4,5 +4,23 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Teams {
-    private final List<User> teamMembers = new ArrayList<>();
+    private final List<User> teamMembers;
+    private int teamScore;
+
+    public Teams(List<User> teamMembers, int teamScore) {
+        this.teamMembers = teamMembers;
+        this.teamScore = teamScore;
+    }
+
+    public List<User> getTeamMembers() {
+        return teamMembers;
+    }
+
+    public int getTeamScore() {
+        return teamScore;
+    }
+
+    public void setTeamScore(int teamScore) {
+        this.teamScore = teamScore;
+    }
 }
--- KahIT/app/src/main/java/com/god/kahit/model/User.java
+++ KahIT/app/src/main/java/com/god/kahit/model/User.java
@@ -1,11 +1,27 @@
 package com.god.kahit.model;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class User {
     private String name;
+    private int score;
+    private List<Buyable> items;
 
-    private final List<Powerups> powerups = new ArrayList<>();
-    private final List<Cosmetic> cosmetics = new ArrayList<>();
+    public User(String name, int score, List<Buyable> items) {
+        this.name = name;
+        this.score = score;
+        this.items = items;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public List<Buyable> getItems() {
+        return items;
+    }
 }
