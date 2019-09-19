5081129bd6b342986d164933a01e3f533c260f0c
MiztaOak <ek.johan98@gmail.com>
2019-09-19 19:22:55 +0200

    Added documentation to the QuestionFactory

--- KahIT/app/src/main/java/com/god/kahit/model/QuestionFactory.java
+++ KahIT/app/src/main/java/com/god/kahit/model/QuestionFactory.java
@@ -5,8 +5,16 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+/**
+ * A factory class used to create questions
+ */
 public class QuestionFactory {
 
+    /**
+     * A method that creates Map of questions with category as the key and a list of questions as the values
+     * @param categories A array of the categories that will be inside the map
+     * @return The map with category as the key and a list of questions as the values
+     */
     public static Map<Category, List<Question>> getQuestionMap(Category[] categories){
         Map<Category,List<Question>> questions = new HashMap<>();
 
@@ -17,6 +25,11 @@ public class QuestionFactory {
         return questions;
     }
 
+    /**
+     * Method that creates a list of question based on a given category
+     * @param category the category that the list will focus on
+     * @return the list of questions
+     */
     private static List<Question> getQuestionList(Category category){
         List<Question> questionList = new ArrayList<>();
         QuestionDataLoader dataLoader = new QuestionDataLoader("testQuestionDataBase.txt");
