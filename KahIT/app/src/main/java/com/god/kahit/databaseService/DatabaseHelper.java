30a1b267b0a85111e327712ee8e328c80620f1a2
Johan Ek <Ek.Johan98@gmail.com>
2019-09-24 17:32:02 +0200

    Added documentation to the DatabaseHelper class

--- KahIT/app/src/main/java/com/god/kahit/databaseService/DatabaseHelper.java
+++ KahIT/app/src/main/java/com/god/kahit/databaseService/DatabaseHelper.java
@@ -12,6 +12,10 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 
+/**
+ * Helper class for the SQLite database that is used to copy the database from the assets folder to
+ * the correct location as well as manage it.
+ */
 public class DatabaseHelper extends SQLiteOpenHelper {
     private static String DB_NAME = "questions.db";
     private static String DB_PATH = "";
@@ -36,6 +40,10 @@ public class DatabaseHelper extends SQLiteOpenHelper {
         this.getReadableDatabase();
     }
 
+    /**
+     * Method that updates the database using the copyDataBase() method
+     * @throws IOException
+     */
     public void updateDataBase() throws IOException{
         if(mNeedUpdate || !hasCheckedForNewData){
             File dbFile = new File(DB_PATH + DB_NAME);
@@ -54,6 +62,10 @@ public class DatabaseHelper extends SQLiteOpenHelper {
         return dbFile.exists();
     }
 
+    /**
+     * Method that copies the database from the assets folder to the appropriate folder for androids
+     * structure
+     */
     private void copyDataBase(){
         if(!checkDataBase()){
             this.getReadableDatabase();
@@ -66,6 +78,10 @@ public class DatabaseHelper extends SQLiteOpenHelper {
         }
     }
 
+    /**
+     * Method that copies the database file from the assets folder
+     * @throws IOException
+     */
     private void copyDBFile() throws IOException{
         InputStream mInput = mContext.getAssets().open(DB_NAME);
         OutputStream mOutput = new FileOutputStream(DB_PATH + DB_NAME);
@@ -79,11 +95,19 @@ public class DatabaseHelper extends SQLiteOpenHelper {
         mInput.close();
     }
 
+    /**
+     * Method that opens the database
+     * @return returns true if the database was opened
+     * @throws SQLException
+     */
     public boolean openDataBase() throws SQLException {
         mDataBase = SQLiteDatabase.openDatabase(DB_PATH + DB_NAME, null, SQLiteDatabase.CREATE_IF_NECESSARY);
         return mDataBase != null;
     }
 
+    /**
+     * Method that closes the database
+     */
     @Override
     public synchronized void close(){
         if(mDataBase != null){
@@ -94,7 +118,7 @@ public class DatabaseHelper extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase sqLiteDatabase) {}
-
+    
     @Override
     public void onUpgrade(SQLiteDatabase sqLiteDatabase, int newVersion, int oldVersion) {
         if(newVersion > oldVersion){
