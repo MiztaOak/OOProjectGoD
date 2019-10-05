5a7acf2c5980b5678602aa1f38211e93d7905a03
Maatss <39332053+Maatss@users.noreply.github.com>
2019-10-05 12:41:57 +0200

    Added push_report script

--- KahIT/push_report.sh
+++ KahIT/push_report.sh
@@ -1,10 +1,24 @@
-#!/bin/bash
+#!/bin/sh
 
+setup_git() {
+  git config --global user.email "travis@travis-ci.org"
+  git config --global user.name "Travis CI"
+}
+
+commit_website_files() {
+  git checkout -b gitInspectorReport
+  git add git_inspector_report.html
+  git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
+}
+
+upload_files() {
+  git remote add origin https://${GH_TOKEN}@github.com/MiztaOak/OOProjectGoD.git > /dev/null 2>&1
+  git push --quiet --set-upstream origin gitInspectorReport
+}
+
+echo "Starting script"
 cd ..
-echo "Starting git inspector.."
-
-python git_inspector/gitinspector.py --grading=1 --exclude="git_inspector" --format=html > git_inspector_report.html
-
-echo "Report completed"
-
-pause
+setup_git
+commit_website_files
+upload_files
+echo "Script complete"
