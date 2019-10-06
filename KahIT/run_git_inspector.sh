bbbacbbe0c2c943452a38e644e723d5a78c9fbbb
Maatss <39332053+Maatss@users.noreply.github.com>
2019-10-06 23:41:40 +0200

    Cleaned up travis.yml

--- KahIT/run_git_inspector.sh
+++ KahIT/run_git_inspector.sh
@@ -3,7 +3,7 @@
 cd ..
 echo "Starting git inspector.."
 
-python git_inspector/gitinspector.py --grading=1 --exclude="git_inspector" --format=html > git_inspector_report.html
+python git_inspector/gitinspector.py --grading=1 --exclude="git_inspector" --format=html > index.html
 
 echo "Report completed"
 
