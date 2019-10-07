4b815e3a11db70610be667e2bd317039940c252d
anas <anas-qw@windowslive.com>
2019-10-07 22:59:15 +0200

    Fixed some unused imports in store and added methods for cleaner code

--- KahIT/app/src/main/java/com/god/kahit/view/StoreView.java
+++ KahIT/app/src/main/java/com/god/kahit/view/StoreView.java
@@ -29,9 +29,7 @@ import androidx.lifecycle.MutableLiveData;
 public class StoreView extends AppCompatActivity {
     private StoreViewModel storeViewModel = new StoreViewModel();
     private TextView pointsText, itemType1, itemType2, itemType3;
-    private ArrayList<ImageView> itemsIcons = new ArrayList<>();  //Array list of imageButton
-    private MutableLiveData<Integer> points;
-    private Button itemButton1;
+    private ArrayList<ImageView> itemsIcons = new ArrayList<>();
     private DrawerLayout drawerLayout;
     private ImageView storeImage;
     private List<Button> itemButtons = new ArrayList<>();
@@ -40,11 +38,80 @@ public class StoreView extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.sidenav_store);
-        storeViewModel = ViewModelProviders.of(this).get(StoreViewModel.class);
         initializeStoreView();
-        itemButton1 = findViewById(R.id.itemButton);
+
+    }
+
+    /**
+     * A method that calls all other methods that the view requires.
+     * This method gets called upon creating the view
+     */
+    public void initializeStoreView() {
+        findViews();
+        findItemIcons();
+        findItemButtons();
+        populateItemIcons();
+        setPointsText();
+        setItemTypes();
+        setButtonText();
+        addActionsToTheButtons();
+        addDrawerListener();
+    }
+    /**
+     *A method that iterates through the list of items and get their names and their image source to set them correctly in the view
+     */
+    public void populateItemIcons() {
+        for (int i = 0; i < storeViewModel.getStoreItems().size(); i++) {
+            int resId = getResources().getIdentifier(storeViewModel.getStoreItems().get(i).getImageSource(), "drawable", getPackageName());
+            itemsIcons.get(i).setImageResource(resId);
+        }
+    }
+    /**
+     * A method that finds all the necessary views by their id in order for this view to work
+     */
+    public void findViews(){
         drawerLayout = findViewById(R.id.drawer_layout);
         storeImage = findViewById(R.id.storeImage);
+        pointsText = findViewById(R.id.pointsText);
+        itemType1 = findViewById(R.id.itemType1);
+        itemType2 = findViewById(R.id.itemType2);
+        itemType3 = findViewById(R.id.itemType3);
+    }
+    /**
+     * A method that finds all the imageViews and adds them to a list of imageViews
+     */
+    public void findItemIcons() {
+        itemsIcons.add((ImageView) findViewById(R.id.itemIcon));
+        itemsIcons.add((ImageView) findViewById(R.id.itemIcon2));
+        itemsIcons.add((ImageView) findViewById(R.id.itemIcon3));
+        itemsIcons.add((ImageView) findViewById(R.id.itemIcon4));
+        itemsIcons.add((ImageView) findViewById(R.id.itemIcon5));
+        itemsIcons.add((ImageView) findViewById(R.id.itemIcon6));
+        itemsIcons.add((ImageView) findViewById(R.id.itemIcon7));
+        itemsIcons.add((ImageView) findViewById(R.id.itemIcon8));
+        itemsIcons.add((ImageView) findViewById(R.id.itemIcon9));
+
+    }
+    /**
+     * A method that finds all the buttons and adds them to a list of buttons
+     */
+    public void findItemButtons() {
+
+        itemButtons.add((Button) findViewById(R.id.itemButton)); // Add each imageButton in the view to the array list
+        itemButtons.add((Button) findViewById(R.id.itemButton2));
+        itemButtons.add((Button) findViewById(R.id.itemButton3));
+        itemButtons.add((Button) findViewById(R.id.itemButton4));
+        itemButtons.add((Button) findViewById(R.id.itemButton5));
+        itemButtons.add((Button) findViewById(R.id.itemButton6));
+        //itemButtons.add((Button) findViewById(R.id.itemButton7)); //Empty buttons for now
+        //itemButtons.add((Button) findViewById(R.id.itemButton8)); //Empty buttons for now
+        //itemButtons.add((Button) findViewById(R.id.itemButton9)); //Empty buttons for now
+    }
+    /**
+     * A method that adds an action to the drawer layout which changes the position of storeImage
+     * upon openning and closing
+     */
+    public void addDrawerListener(){
         drawerLayout.addDrawerListener(new DrawerLayout.DrawerListener() {
             @Override
             public void onDrawerSlide(@NonNull View view, float v) {
@@ -71,70 +138,25 @@ public class StoreView extends AppCompatActivity {
             }
         });
     }
-
-    public void initializeStoreView() {
-        pointsText = findViewById(R.id.pointsText);
-        findItemTypes();
-        findItemIcons();
-        findItemButtons();
-        populateItemIcons();
-        setPointsText();
-        setItemTypes();
-        setButtonText();
-        addActionsToTheButtons();
-    }
-
-    public void populateItemIcons() {
-        for (int i = 0; i < storeViewModel.getStoreItems().size(); i++) { // iterate through the list of items and get their names and their image source to set them correctly in the view
-            int resId = getResources().getIdentifier(storeViewModel.getStoreItems().get(i).getImageSource(), "drawable", getPackageName()); //Gets the id of an item's image
-            itemsIcons.get(i).setImageResource(resId); //Sets the image of the imageButton to the image of the item
-        }
-    }
-
-    public void findItemTypes() {
-        itemType1 = findViewById(R.id.itemType1);
-        itemType2 = findViewById(R.id.itemType2);
-        itemType3 = findViewById(R.id.itemType3);
-    }
-
-    public void findItemIcons() {
-
-        itemsIcons.add((ImageView) findViewById(R.id.itemIcon)); // Add each imageButton in the view to the array list
-        itemsIcons.add((ImageView) findViewById(R.id.itemIcon2));
-        itemsIcons.add((ImageView) findViewById(R.id.itemIcon3));
-        itemsIcons.add((ImageView) findViewById(R.id.itemIcon4));
-        itemsIcons.add((ImageView) findViewById(R.id.itemIcon5));
-        itemsIcons.add((ImageView) findViewById(R.id.itemIcon6));
-        itemsIcons.add((ImageView) findViewById(R.id.itemIcon7));
-        itemsIcons.add((ImageView) findViewById(R.id.itemIcon8));
-        itemsIcons.add((ImageView) findViewById(R.id.itemIcon9));
-
-    }
-
-    public void findItemButtons() {
-
-        itemButtons.add((Button) findViewById(R.id.itemButton)); // Add each imageButton in the view to the array list
-        itemButtons.add((Button) findViewById(R.id.itemButton2));
-        itemButtons.add((Button) findViewById(R.id.itemButton3));
-        itemButtons.add((Button) findViewById(R.id.itemButton4));
-        itemButtons.add((Button) findViewById(R.id.itemButton5));
-        itemButtons.add((Button) findViewById(R.id.itemButton6));
-        //itemButtons.add((Button) findViewById(R.id.itemButton7));
-        //itemButtons.add((Button) findViewById(R.id.itemButton8));
-        //itemButtons.add((Button) findViewById(R.id.itemButton9));
-
-    }
-
+    /**
+     * A method that updates the taxt the old the value of a player's points which changes after
+     * buying an item or answering a question
+     */
     public void setPointsText() {
         pointsText.setText("Points:" + storeViewModel.getStoreModel().getPlayer().getScore());
     }
-
+    /**
+     * A method that sets the type of a group of items in their textviews
+     */
     public void setItemTypes() {
         itemType1.setText(storeViewModel.getStoreItems().get(0).getType());
         itemType2.setText(storeViewModel.getStoreItems().get(1).getType());
         itemType3.setText(storeViewModel.getStoreItems().get(2).getType());
     }
-
+    /**
+     * A method that sets an action to each button that has been add to itemButtons list. The action
+     * is a method call that lets the user buy an item
+     */
     public void addActionsToTheButtons() {
 
         for (final Button itemButton : itemButtons) {
@@ -146,12 +168,19 @@ public class StoreView extends AppCompatActivity {
             });
         }
     }
+    /**
+     * A method that lets the user buy an item from store and displays that the item has been
+     * bought by a graphical confirmation and a toast message
+     *
+     * @param itemButton is which button has been clicked which is need in order to buy the
+     * associated item
+     */
     public void buy(Button itemButton){
         if (storeViewModel.isItemBuyable(storeViewModel.getStoreItems().get(itemButtons.indexOf(itemButton)))){
             storeViewModel.buy(itemButtons.indexOf(itemButton));
             Toast toast = Toast.makeText(getApplicationContext(),
-                    "You got a " + storeViewModel.getStoreItems().get(itemButtons.indexOf(itemButton)).getType() + " "+storeViewModel.getStoreItems().get(itemButtons.indexOf(itemButton)).getName(),
-                    Toast.LENGTH_LONG);
+                "You got a " + storeViewModel.getStoreItems().get(itemButtons.indexOf(itemButton)).getType() + " " +storeViewModel.getStoreItems().get(itemButtons.indexOf(itemButton)).getName(),
+                Toast.LENGTH_LONG);
             toast.show();
             itemButton.setEnabled(false);
             itemsIcons.get(itemButtons.indexOf(itemButton)).setImageResource(R.drawable.checkmark);
@@ -159,6 +188,10 @@ public class StoreView extends AppCompatActivity {
             disablebuttons();
         }
     }
+    /**
+     * A method that disables the buttons used to buy items when the player's points are lower than
+     * the price of an item
+     */
     public void disablebuttons(){
         for (int i = 0; i < itemButtons.size(); i++) {
             if(!storeViewModel.isItemBuyable(storeViewModel.getStoreItems().get(i))){
@@ -166,8 +199,9 @@ public class StoreView extends AppCompatActivity {
             }
         }
     }
-
-
+    /**
+     * A method that sets the text of a button to the price of the item associated to it
+     */
     public void setButtonText(){
         for (int i = 0; i < itemButtons.size(); i++) {
             itemButtons.get(i).setText(Integer.toString(storeViewModel.getStoreItems().get(i).getPrice()));
--- KahIT/app/src/main/java/com/god/kahit/viewModel/StoreViewModel.java
+++ KahIT/app/src/main/java/com/god/kahit/viewModel/StoreViewModel.java
@@ -5,13 +5,11 @@ import com.god.kahit.model.Store;
 
 import java.util.List;
 
-import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
 public class StoreViewModel extends ViewModel {
     public int i = 0;
     private Store storeModel = new Store();
-    //private MutableLiveData<Integer> points = new MutableLiveData<>();
     public boolean isItemBuyable(Item item){
         return (!(item.getPrice() > storeModel.getPlayer().getScore()));
     }
@@ -31,17 +29,4 @@ public class StoreViewModel extends ViewModel {
     public void setStoreModel(Store storeModel) {
         this.storeModel = storeModel;
     }
-//TODO cheak mutableLiveDate
-/*
-    public MutableLiveData<Integer> getPoints() {
-        return points;
-    }
-    public void setPoints(int p) {
-        if ((points.getValue() == (null))) {
-            this.points.setValue(p);
-        } else {
-            this.points.setValue(points.getValue() + p);
-        }
-    }
-*/
 }
