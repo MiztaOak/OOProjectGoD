56f498952127932b2514e64e7adb279ca84ba315
Maatss <39332053+Maatss@users.noreply.github.com>
2019-10-08 09:00:34 +0200

    Altered generate_javadoc script Unmuted javadoc output

--- KahIT/generate_javadoc.sh
+++ KahIT/generate_javadoc.sh
@@ -3,7 +3,8 @@
 cd ..
 echo "Starting to generate javadoc.."
 
-javadoc -protected -splitindex -d JavaDoc -sourcepath KahIT/app/src/main/java -subpackages com.god.kahit -Xdoclint:none >/dev/null 2>&1 # Suppresses normal logs and error log 
+# javadoc -protected -splitindex -d JavaDoc -sourcepath KahIT/app/src/main/java -subpackages com.god.kahit -Xdoclint:none >/dev/null 2>&1 # Suppresses normal logs and error log 
+javadoc -protected -splitindex -d JavaDoc -sourcepath KahIT/app/src/main/java -subpackages com.god.kahit -Xdoclint:none
 
 echo "JavaDoc completed, forcing exit with 0"
 exit 0
