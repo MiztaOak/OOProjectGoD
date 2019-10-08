0dd680ce06bd42086f97b680134380e511e873cc
Mats <peterpirat95@hotmail.com>
2019-10-08 15:50:57 +0200

    Extracted team selection spinner from recycler view Put the spinner inside hotswap_add_players.xml instead to reduce complexity

--- KahIT/app/src/main/java/com/god/kahit/view/HotSwapAddPlayersView.java
+++ KahIT/app/src/main/java/com/god/kahit/view/HotSwapAddPlayersView.java
@@ -4,7 +4,16 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
+import android.widget.Spinner;
+
+import com.god.kahit.R;
+import com.god.kahit.ViewModel.HotSwapAddPlayersViewModel;
+import com.god.kahit.model.Player;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,13 +24,7 @@ import androidx.lifecycle.ViewModelProviders;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import com.god.kahit.R;
-import com.god.kahit.ViewModel.HotSwapAddPlayersViewModel;
-import com.god.kahit.model.Player;
-
-import java.util.List;
-
-public class HotSwapAddPlayersView extends AppCompatActivity implements IOnPlayerClickListener {
+public class HotSwapAddPlayersView extends AppCompatActivity implements IOnPlayerClickListener, AdapterView.OnItemSelectedListener {
 
     private static final String LOG_TAG = HotSwapAddPlayersView.class.getSimpleName();
     RecyclerView.LayoutManager layoutManager;
@@ -29,6 +32,10 @@ public class HotSwapAddPlayersView extends AppCompatActivity implements IOnPlaye
     HotSwapAddPlayersViewModel hotSwapAddPlayersViewModel;
     private RecyclerView recyclerView;
     private RecyclerView.Adapter recyclerAdapter;
+
+    Spinner spin;
+    List<Integer> teamColors;
+    List<String> teamNumbers;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,6 +53,7 @@ public class HotSwapAddPlayersView extends AppCompatActivity implements IOnPlaye
         });
 
         setupRecyclerView();
+        setupSpinner();
 
         Button addTeamButton = findViewById(R.id.addTeamButton);
 
@@ -55,6 +63,40 @@ public class HotSwapAddPlayersView extends AppCompatActivity implements IOnPlaye
                 hotSwapAddPlayersViewModel.addNewPlayer();
             }
         });
+    }
+
+    private void setupSpinner() {
+        spin = findViewById(R.id.spinnerChangeTeam);
+
+        initTeamColors();
+        initTeamNumbers();
+
+        spin.setAdapter(new CustomSpinnerAdapter(
+                getApplicationContext(),
+                R.layout.spinner_item,
+                R.id.text1,
+                teamNumbers,
+                teamColors));
+
+        spin.setSelection(0);
+//        spin.setBackgroundColor(teamColors.get(spin.getSelectedItemPosition()));
+
+        spin.setOnItemSelectedListener(this);
+    }
+
+    private void initTeamNumbers() {
+        teamNumbers = new ArrayList<>();
+        for (int i = 1; i < 9; i++) {
+            teamNumbers.add(" " + i + " ");
+        }
+    }
+
+    private void initTeamColors() {
+        teamColors = new ArrayList<>();
+        int[] retrieve = getApplicationContext().getResources().getIntArray(R.array.androidcolors);
+        for (int re : retrieve) {
+            teamColors.add(re);
+        }
     }
 
     /**
@@ -95,5 +137,15 @@ public class HotSwapAddPlayersView extends AppCompatActivity implements IOnPlaye
         Log.d(LOG_TAG, "Button clicked!");
         Intent intent = new Intent(this, QuestionClass.class);
         startActivity(intent);
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        System.out.println("HotSwapAddPlayersView - onItemSelected: Called");
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+        System.out.println("HotSwapAddPlayersView - onNothingSelected: Called");
     }
 }
