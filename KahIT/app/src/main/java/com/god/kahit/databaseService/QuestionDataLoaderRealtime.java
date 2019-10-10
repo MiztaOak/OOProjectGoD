7393fe307d167672362e18c634a9c4d1e1980895
Johan Ek <ekjoh@student.chalmer.se>
2019-10-10 15:21:38 +0200

    Made the program keep a lokal copy of the database

--- KahIT/app/src/main/java/com/god/kahit/databaseService/QuestionDataLoaderRealtime.java
+++ KahIT/app/src/main/java/com/god/kahit/databaseService/QuestionDataLoaderRealtime.java
@@ -45,6 +45,7 @@ public class QuestionDataLoaderRealtime implements IQuestionDataLoader {
         FirebaseApp.initializeApp(context);
         db = FirebaseDatabase.getInstance();
         databaseReference = db.getReference("questions");
+        databaseReference.keepSynced(true);
         questions = new HashMap<>();
         loadData();
     }
