019468fb11b3e32df1805b4b69c75871b738b4f2
anas <anas-qw@windowslive.com>
2019-10-15 12:25:41 +0200

    Added more JavaDoc to my classes

--- KahIT/app/src/main/java/com/god/kahit/model/Item.java
+++ KahIT/app/src/main/java/com/god/kahit/model/Item.java
@@ -1,5 +1,15 @@
 package com.god.kahit.model;
-
+/**
+ * @responsibility: This class is responsible for all items which can be bought by players.
+ * This class holds all the common values between different items like buffs,
+ * debuffs and vanity items
+ *
+ * @used-by: This class is used in the following classes:
+ * QuizGame, Store, IItemDataLoader, ItemDataLoaderRealTime, Lottery, LotteryViewModel, LotteryView
+ * Modifier, Repository, VanityItem, StoreViewModel and in the database.
+ *
+ * @author: Anas Alkoutli
+ */
 public abstract class Item {
     private int price;
     private String name;
--- KahIT/app/src/main/java/com/god/kahit/model/Store.java
+++ KahIT/app/src/main/java/com/god/kahit/model/Store.java
@@ -1,7 +1,15 @@
 package com.god.kahit.model;
 
 import java.util.List;
-
+/**
+ * @responsibility: This class is responsible for
+ *
+ * @used-by: This class is used in the following classes:
+ * QuizGame, Store, IItemDataLoader, ItemDataLoaderRealTime, Lottery, LotteryViewModel, LotteryView
+ * Modifier, Repository, VanityItem, StoreViewModel and in the database.
+ *
+ * @author: Anas Alkoutli
+ */
 public class Store { //todo implement a method to restock store?
     private List<Item> storeItems;
     private Player player; //todo remove local instance of player
