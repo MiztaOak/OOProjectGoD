6a13dc6c1839318438e721afe78d9bfda8992606
Oussama <Anadani>
2019-10-16 00:56:12 +0200

    Initialized musicSevice and tried to start/stop music in settings page

--- KahIT/app/src/main/java/com/god/kahit/view/MainActivityView.java
+++ KahIT/app/src/main/java/com/god/kahit/view/MainActivityView.java
@@ -1,109 +1,122 @@
 package com.god.kahit.view;
 
-import android.Manifest;
+import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
-import android.content.pm.PackageManager;
+import android.content.ServiceConnection;
 import android.os.Bundle;
+import android.os.IBinder;
 import android.util.Log;
 import android.view.View;
-import android.widget.Toast;
+
 
 import com.god.kahit.R;
 import com.god.kahit.Repository;
+import com.god.kahit.backgroundMusicService.MusicService;
 import com.google.firebase.database.FirebaseDatabase;
 
-import androidx.annotation.CallSuper;
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.content.ContextCompat;
 
 public class MainActivityView extends AppCompatActivity {
     private static final String LOG_TAG = MainActivityView.class.getSimpleName();
 
-    private static final String[] REQUIRED_PERMISSIONS =
-            new String[]{
-                    Manifest.permission.BLUETOOTH,
-                    Manifest.permission.BLUETOOTH_ADMIN,
-                    Manifest.permission.ACCESS_WIFI_STATE,
-                    Manifest.permission.CHANGE_WIFI_STATE,
-                    Manifest.permission.ACCESS_COARSE_LOCATION,
-            };
+    private boolean mIsBound = false;
+    private MusicService mServ;
 
-    private static final int REQUEST_CODE_REQUIRED_PERMISSIONS = 1;
-
-    /**
-     * Returns true if the app was granted all the permissions. Otherwise, returns false.
-     */
-    private static boolean hasPermissions(Context context, String... permissions) {
-        for (String permission : permissions) {
-            if (ContextCompat.checkSelfPermission(context, permission)
-                    != PackageManager.PERMISSION_GRANTED) {
-                return false;
-            }
-        }
-        return true;
-    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        FirebaseDatabase.getInstance().setPersistenceEnabled(true);
         setContentView(R.layout.main_activity);
         Repository.getInstance().startNewGameInstance(getApplicationContext());
-    }
 
-    @Override
-    protected void onStart() {
-        super.onStart();
-        if (!hasPermissions(this, REQUIRED_PERMISSIONS)) {
-            requestPermissions(REQUIRED_PERMISSIONS, REQUEST_CODE_REQUIRED_PERMISSIONS);
-        }
-    }
+        doBindService();
+        Intent music = new Intent();
+        music.setClass(this, MusicService.class);
+        startService(music);
 
-    /**
-     * Handles user acceptance (or denial) of our permission request.
-     */
-    @CallSuper
-    @Override
-    public void onRequestPermissionsResult(
-            int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-
-        if (requestCode != REQUEST_CODE_REQUIRED_PERMISSIONS) {
-            return;
-        }
-
-        for (int grantResult : grantResults) {
-            if (grantResult == PackageManager.PERMISSION_DENIED) {
-                Toast.makeText(this, "Cannot start without required permissions", Toast.LENGTH_LONG).show();
-                finish();
-                return;
-            }
-        }
-        recreate();
     }
 
     public void launchChooseGameClass(View view) {
         Log.d(LOG_TAG, "Button clicked!");
+//        Intent intent = new Intent(this, ChooseGameView.class);
         Intent intent = new Intent(this, ChooseGameView.class);
         startActivity(intent);
     }
-
     public void launchSettingsView(View view) {
         Log.d(LOG_TAG, "Button clicked!");
+//        Intent intent = new Intent(this, ChooseGameClass.class);
         Intent intent = new Intent(this, SettingsView.class);
         startActivity(intent);
     }
-
     public void launchPreGameCountdownView(View view) {
         Log.d(LOG_TAG, "Button clicked!");
+//        Intent intent = new Intent(this, ChooseGameClass.class);
         Intent intent = new Intent(this, PreGameCountdownView.class);
         startActivity(intent);
     }
-
     public void launchAboutKahitView(View view) {
         Log.d(LOG_TAG, "Button clicked!");
+//        Intent intent = new Intent(this, ChooseGameClass.class);
         Intent intent = new Intent(this, AboutKahitView.class);
         startActivity(intent);
     }
+
+   // public void onPause() { super.onPause(); if (mp != null) mp.pause(); }
+  /* @Override
+   protected void onResume() {
+       if(mp != null && !mp.isPlaying())
+           mp.start();
+       super.onResume();
+   }*/
+
+    private ServiceConnection Scon =new ServiceConnection(){
+
+        public void onServiceConnected(ComponentName name, IBinder
+                binder) {
+            mServ = ((MusicService.ServiceBinder)binder).getService();
+        }
+
+        public void onServiceDisconnected(ComponentName name) {
+            mServ = null;
+        }
+    };
+
+    void doBindService(){
+        bindService(new Intent(this,MusicService.class),
+                Scon,Context.BIND_AUTO_CREATE);
+        mIsBound = true;
+    }
+
+    void doUnbindService()
+    {
+        if(mIsBound)
+        {
+            unbindService(Scon);
+            mIsBound = false;
+        }
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        if (mServ != null) {
+            mServ.resumeMusic();
+        }
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        doUnbindService();
+        Intent music = new Intent();
+        music.setClass(this,MusicService.class);
+        stopService(music);
+
+    }
+
+
 }
--- KahIT/app/src/main/java/com/god/kahit/view/SettingsView.java
+++ KahIT/app/src/main/java/com/god/kahit/view/SettingsView.java
@@ -1,18 +1,17 @@
 package com.god.kahit.view;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.annotation.SuppressLint;
+import android.media.MediaPlayer;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.CompoundButton;
 import android.widget.Switch;
 import android.widget.Toast;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.god.kahit.R;
 
 public class SettingsView extends AppCompatActivity {
-
+    MediaPlayer m1 = null;
     Switch musicSwitch;
 
     @Override
@@ -30,18 +29,26 @@ public class SettingsView extends AppCompatActivity {
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
                 if (musicSwitch.isChecked()) {
                     // turn music on
+                    stopPlaying();
+                    m1 = MediaPlayer.create(SettingsView.this, R.raw.pre_game_song);
+                    m1.start();
                     Toast.makeText(getApplicationContext(), "Music on", Toast.LENGTH_SHORT).show();
-                }
-                else {
+                } else {
+                    stopPlaying();
                     Toast.makeText(getApplicationContext(), "Music off", Toast.LENGTH_SHORT).show();
                     //turn music off
                 }
             }
         });
-
     }
 
+    private void stopPlaying() {
+        if (m1 != null) {
+            m1.stop();
+            m1.release();
+            m1 = null;
+        }
 
 
-
+    }
 }
