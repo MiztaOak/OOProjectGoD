80d49aa38b776a9f70819e5dd32b9d0608dcd6e6
Johan Ek <ekjoh@student.chalmer.se>
2019-10-17 15:12:07 +0200

    Added ids to category and item Both category and item have ids now. The categories have the method getId that returns their id and item hold their id as a string which is gotten from the database.

--- KahIT/app/src/main/java/com/god/kahit/model/VanityItem.java
+++ KahIT/app/src/main/java/com/god/kahit/model/VanityItem.java
@@ -2,7 +2,7 @@ package com.god.kahit.model;
 
 public class VanityItem extends Item { //todo Simply to give this class any real meaning, maybe implement a how-many-rounds-it-lasts variable?
 
-    public VanityItem(int price, String name) {
-        super(price, name);
+    public VanityItem(int price, String name, String id) {
+        super(price, name, id);
     }
 }
