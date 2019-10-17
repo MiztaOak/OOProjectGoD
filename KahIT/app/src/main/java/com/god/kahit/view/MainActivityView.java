ed57877fe45af8bf15d9b0dae6c267976822df00
Oussama <Anadani>
2019-10-17 11:30:18 +0200

    Fixed Music that it pauses when home button clicked part2

--- KahIT/app/src/main/java/com/god/kahit/view/MainActivityView.java
+++ KahIT/app/src/main/java/com/god/kahit/view/MainActivityView.java
@@ -1,25 +1,34 @@
 package com.god.kahit.view;
 
+
 import android.Manifest;
+import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
+import android.content.ServiceConnection;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.os.IBinder;
+import android.os.PowerManager;
 import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
-
-import com.god.kahit.R;
-import com.god.kahit.Repository.Repository;
-import com.google.firebase.database.FirebaseDatabase;
 
 import androidx.annotation.CallSuper;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
 
+import com.god.kahit.R;
+import com.god.kahit.Repository;
+import com.god.kahit.backgroundMusicService.HomeButtonClickedListener;
+import com.god.kahit.backgroundMusicService.MusicService;
+import com.god.kahit.backgroundMusicService.OnHomePressedListener;
+
 public class MainActivityView extends AppCompatActivity {
     private static final String LOG_TAG = MainActivityView.class.getSimpleName();
+    HomeButtonClickedListener mHomeWatcher;
+
 
     private static final String[] REQUIRED_PERMISSIONS =
             new String[]{
@@ -31,6 +40,18 @@ public class MainActivityView extends AppCompatActivity {
             };
 
     private static final int REQUEST_CODE_REQUIRED_PERMISSIONS = 1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_activity);
+        Repository.getInstance().startNewGameInstance(getApplicationContext());
+        
+        startMusic();
+        startHomeButtonListener();
+    }
+
+
 
     /**
      * Returns true if the app was granted all the permissions. Otherwise, returns false.
@@ -45,14 +66,7 @@ public class MainActivityView extends AppCompatActivity {
         return true;
     }
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        FirebaseDatabase.getInstance().setPersistenceEnabled(true);
-        setContentView(R.layout.main_activity);
-        Repository.getInstance().startNewGameInstance(getApplicationContext());
-        Repository.getInstance().setupAppLifecycleObserver(getApplicationContext());
-    }
+
 
     @Override
     protected void onStart() {
@@ -60,7 +74,6 @@ public class MainActivityView extends AppCompatActivity {
         if (!hasPermissions(this, REQUIRED_PERMISSIONS)) {
             requestPermissions(REQUIRED_PERMISSIONS, REQUEST_CODE_REQUIRED_PERMISSIONS);
         }
-        Repository.getInstance().resetApp();
     }
 
     /**
@@ -98,9 +111,132 @@ public class MainActivityView extends AppCompatActivity {
         startActivity(intent);
     }
 
+    public void launchPreGameCountdownView(View view) {
+        Log.d(LOG_TAG, "Button clicked!");
+        Intent intent = new Intent(this, PreGameCountdownView.class);
+        startActivity(intent);
+    }
+
     public void launchAboutKahitView(View view) {
         Log.d(LOG_TAG, "Button clicked!");
         Intent intent = new Intent(this, AboutKahitView.class);
         startActivity(intent);
     }
+
+
+
+
+    public void startMusic() {
+
+        //BIND Music Service
+        doBindService();
+        Intent music = new Intent();
+        music.setClass(this, MusicService.class);
+        startService(music);
+    }
+
+
+    private void startHomeButtonListener() {
+        //Start HomeButtonClickedListener
+        mHomeWatcher = new HomeButtonClickedListener(this);
+        mHomeWatcher.setOnHomePressedListener(new OnHomePressedListener() {
+            @Override
+            public void onHomePressed() {
+                if (mServ != null) {
+                    mServ.pauseMusic();
+                }
+            }
+
+            @Override
+            public void onHomeLongPressed() {
+                if (mServ != null) {
+                    mServ.pauseMusic();
+                }
+            }
+        });
+        mHomeWatcher.start();
+    }
+
+
+
+    // public void onPause() { super.onPause(); if (mp != null) mp.pause(); }
+  /* @Override
+   protected void onResume() {
+       if(mp != null && !mp.isPlaying())
+           mp.start();
+       super.onResume();
+   }*/
+
+
+    //Bind/Unbind music service
+    private boolean mIsBound = false;
+    private MusicService mServ;
+    private ServiceConnection Scon = new ServiceConnection() {
+
+        public void onServiceConnected(ComponentName name, IBinder
+                binder) {
+            mServ = ((MusicService.ServiceBinder) binder).getService();
+        }
+
+        public void onServiceDisconnected(ComponentName name) {
+            mServ = null;
+        }
+    };
+
+
+    void doBindService() {
+        bindService(new Intent(this, MusicService.class),
+                Scon, Context.BIND_AUTO_CREATE);
+        mIsBound = true;
+    }
+
+    void doUnbindService() {
+        if (mIsBound) {
+            unbindService(Scon);
+            mIsBound = false;
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        if (mServ != null) {
+            mServ.resumeMusic();
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        //Detect idle screen
+        PowerManager pm = (PowerManager)
+                getSystemService(Context.POWER_SERVICE);
+        boolean isScreenOn = false;
+        if (pm != null) {
+            isScreenOn = pm.isScreenOn();
+        }
+
+        if (!isScreenOn) {
+            if (mServ != null) {
+                mServ.pauseMusic();
+            }
+        }
+    }
+
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        //UNBIND music service
+        doUnbindService();
+        Intent music = new Intent();
+        music.setClass(this, MusicService.class);
+        stopService(music);
+
+    }
+
+
 }
