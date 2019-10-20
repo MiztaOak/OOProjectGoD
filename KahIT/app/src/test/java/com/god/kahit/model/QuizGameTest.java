c856e9f208fcee1da481e1a9f730d3799c85bdf2
Johan Ek <ekjoh@student.chalmer.se>
2019-10-20 16:12:41 +0200

    Wrote the tests for the store

---
+++ KahIT/app/src/test/java/com/god/kahit/model/QuizGameTest.java
@@ -0,0 +1,125 @@
+package com.god.kahit.model;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class QuizGameTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    public void startGame() {
+    }
+
+    @Test
+    public void endGame() {
+    }
+
+    @Test
+    public void startRound() {
+    }
+
+    @Test
+    public void nextQuestion() {
+    }
+
+    @Test
+    public void enterAnswer() {
+    }
+
+    @Test
+    public void isRoundOver() {
+    }
+
+    @Test
+    public void addNewPlayerToTeam() {
+    }
+
+    @Test
+    public void addNewPlayerToEmptyTeam() {
+    }
+
+    @Test
+    public void addNewPlayerToEmptyTeam1() {
+    }
+
+    @Test
+    public void noEmptyTeamExists() {
+    }
+
+    @Test
+    public void getEmptyTeam() {
+    }
+
+    @Test
+    public void createNewTeam() {
+    }
+
+    @Test
+    public void createNewTeam1() {
+    }
+
+    @Test
+    public void createNewPlayer() {
+    }
+
+    @Test
+    public void createNewPlayer1() {
+    }
+
+    @Test
+    public void removePlayer() {
+    }
+
+    @Test
+    public void removeTeamIfEmpty() {
+    }
+
+    @Test
+    public void changeTeamName() {
+    }
+
+    @Test
+    public void changePlayerName() {
+    }
+
+    @Test
+    public void getTotalAmountOfPlayers() {
+    }
+
+    @Test
+    public void resetPlayerData() {
+    }
+
+    @Test
+    public void changeTeam() {
+    }
+
+    @Test
+    public void changeTeam1() {
+    }
+
+    @Test
+    public void checkAllPlayersReady() {
+    }
+
+    @Test
+    public void setIsPlayerReady() {
+    }
+
+    @Test
+    public void applyModifier() {
+    }
+
+    @Test
+    public void getAllItems() {
+    }
+
+    @Test
+    public void incrementCurrentPlayer() {
+    }
+}
