24aed6cd54dcd1da3815adc7e7e780771abb095c
anas <anas-qw@windowslive.com>
2019-10-25 19:46:23 +0200

    Added javaDoc to events

--- KahIT/app/src/main/java/com/god/kahit/applicationEvents/GameJoinedLobbyEvent.java
+++ KahIT/app/src/main/java/com/god/kahit/applicationEvents/GameJoinedLobbyEvent.java
@@ -1,4 +1,11 @@
 package com.god.kahit.applicationEvents;
+/**
+ * @responsibility: This class is responsible for handling information when joining a lobby.
+ * The event is fired when a player has joined a lobby and is not a host.
+ * @used-by: This class is used in the following classes:
+ * Repository and JoinLobbyNetView
+ * @author: Anas Alkoutli
+ */
 
 import com.god.kahit.model.IEvent;
 
